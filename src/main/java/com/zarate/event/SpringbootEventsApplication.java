468eb2bd76ff20c083d9737dba3a3118964cd620
joaozarate <joaozarate@gmail.com>
2020-04-14 22:35:24 +0100

    Project created

---
+++ src/main/java/com/zarate/event/SpringbootEventsApplication.java
@@ -0,0 +1,13 @@
+package com.zarate.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zarate/event/SpringbootEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zarate.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
